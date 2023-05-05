34b03c569fa9eef61939fa932ade88e90772f14a
sandhanaK2002 <sandhanakrishnan010@gmail.com>
2023-05-05 14:42:27 +0530

    Animal Page

--- src/App.js
+++ src/App.js
@@ -1,44 +1,30 @@
-import {ProfileCard} from "./ProfileCard"
-import alexa from "./images/alexa.png"
-import cortana from "./images/cortana.png"
-import siri from "./images/siri.png"
+import './App.css';
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
 
-export const App = () =>{
- return (
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
 
-
-    <div className="container">
-    <section className="section">
-      <div className="columns">
-        <div className="column is-4">
-          <ProfileCard
-            title="Alexa"
-            handle="@alexa99"
-            image={alexa}
-            description="Alexa was created by Amazon and helps you buy things."
-          />
-        </div>
-        <div className="column is-4">
-          <ProfileCard
-            title="Cortana"
-            handle="@cortana32"
-            image={cortana}
-            description="Cortana was made by Microsoft. Who knows what it does?"
-          />
-        </div>
-        <div className="column is-4">
-          <ProfileCard
-            title="Siri"
-            handle="@siri01"
-            image={siri}
-            description="Siri was made by Apple and is being phased out"
-          />
-        </div>
-      </div>
-    </section>
-  </div>
-
- )
+  return animals[Math.floor(Math.random() * animals.length)];
 }
 
+function App() {
+  const [animals, setAnimals] = useState([]);
 
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow key={index} type={animal} />;
+  });
+
+  return (
+    <div className="app">
+      <button onClick={handleClick}>Add Animal</button>
+      <div className="animal-list">{renderedAnimals}</div>
+    </div>
+  );
+}
+
+export default App;
